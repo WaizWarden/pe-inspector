fb5ad0135912560c652a4f10619b448484738401
lukas-notebook <ljurica506@gmail.com>
2026-05-18 21:18:51 +0200

    feat: parsing IAT

--- inspector.py
+++ inspector.py
@@ -1,26 +1,69 @@
-import pefile
 import argparse
 import math
+import pefile
+
 
 class Inspector:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument("filename", help="The name of the file to parse")
         self.args = self.parser.parse_args()
+        self.pe = None
 
-    def parse_file(self):
-
+    def load_file(self):
         try:
-            pe = pefile.PE(self.args.filename)
+            self.pe = pefile.PE(self.args.filename)
 
-            for section in pe.sections:
-                data = section.get_data()
-                name = section.Name.decode(errors='replace').strip('\x00')
-                size = len(data)
-                print(f"{name}:       ENTROPY={Inspector.entropy(data):.2f}     SIZE={size}B")
+        except FileNotFoundError:
+            print(f"Error: File not found at {self.args.filename}")
+            exit(1)
+
+        except pefile.PEFormatError:
+            print(f"Error: Invalid PE file format")
+            exit(1)
 
         except Exception as e:
-            print(f"Exception occurred while loading file: {e}")
+            print(f"Unexpected error while loading file: {e}")
+            exit(1)
+
+
+    def parse_sections_for_entropy(self):
+        try:
+            for section in self.pe.sections:
+                data = section.get_data()
+                name = section.Name.decode(errors='ignore').strip('\x00')
+                entropy = Inspector.entropy(data)
+                size = len(data)
+                print(f"{name}")
+                print(f"    ENTROPY : {entropy:.2f}")
+                print(f"    SIZE : {size}B\n")
+
+        except Exception as e:
+            print(f"Exception occurred while parsing sections: {e}")
+            exit(1)
+
+    def parse_iat(self):
+        try:
+            self.pe.parse_data_directories()
+
+            if not hasattr(self.pe, 'DIRECTORY_ENTRY_IMPORT'):
+                print(f"No IAT found")
+                return
+
+            for entry in self.pe.DIRECTORY_ENTRY_IMPORT:
+                dll_name = entry.dll.decode('utf-8', errors='ignore')
+                print(f"--- {dll_name} ---")
+
+                for imp in entry.imports:
+                    if imp.name:
+                        func_name = imp.name.decode('utf-8', errors='ignore')
+                    else:
+                        func_name = f"Ordinal {imp.ordinal}"
+
+                    print(f"{func_name}")
+
+        except Exception as e:
+            print(f"Exception occurred while parsing IAT: {e}")
             exit(1)
 
     @staticmethod
@@ -37,4 +80,6 @@ class Inspector:
 
 if __name__ == '__main__':
     inspector = Inspector()
-    inspector.parse_file()
+    inspector.load_file()
+    inspector.parse_sections_for_entropy()
+    inspector.parse_iat()
